7faeea924643e20e2b049c1b38eee185a3ca93f5
Sopanha <129533258+SuonSopanha@users.noreply.github.com>
2024-08-18 03:35:02 +0700

    add sidebarMenu component

--- pages/feed/index.vue
+++ pages/feed/index.vue
@@ -1,143 +1,9 @@
+
+
 <template>
     <div class="flex flex-wrap justify-center mx-6">
         <div class="w-full md:w-1/4 p-4 border-r border-gray-300 flex flex-col space-y-3">
-            <div class="sticky top-0">
-
-                <div class="text-sm leading-6 bg-slate-100">
-                    <figure class="relative flex flex-col-reverse bg-slate-100 rounded-lg p-6">
-                        <figcaption class="flex items-center space-x-4">
-                            <img src="https://images.unsplash.com/photo-1632910121591-29e2484c0259?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw4fHxjb2RlcnxlbnwwfDB8fHwxNzEwMTY0NjIzfDA&ixlib=rb-4.0.3&q=80&w=1080"
-                                alt="" class="flex-none w-14 h-14 rounded-full object-cover" loading="lazy"
-                                decoding="async">
-                            <div class="flex-auto">
-                                <div class="text-base text-slate-900 font-semibold ">
-                                    John Doe
-                                </div>
-                                <div class="mt-0.5 ">
-                                    Web Developer
-                                </div>
-                            </div>
-                        </figcaption>
-                    </figure>
-                </div>
-
-                <div class="px-6 py-4 mb-6 bg-slate-100 rounded-lg shadow-lg">
-                    <ul class="flex flex-col gap-6 pl-4 mt-4">
-                        <li class="flex items-center gap-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="w-6 h-6 text-teal-500">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z">
-                                </path>
-                            </svg>
-                            <a href="http://127.0.0.1:8000/user/dashboard"
-                                class="text-lg font-medium text-gray-700 hover:text-teal-500 transition-colors duration-200">
-                                Dashboard
-                            </a>
-                        </li>
-
-                        <li class="flex items-center gap-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="w-6 h-6 text-teal-500">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z">
-                                </path>
-                            </svg>
-                            <a href="http://127.0.0.1:8000/user/study-lists"
-                                class="text-lg font-medium text-gray-700 hover:text-teal-500 transition-colors duration-200">
-                                Study Lists
-                            </a>
-                        </li>
-
-                        <li class="flex items-center gap-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="w-6 h-6 text-teal-500">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m6.75 12l-3-3m0 0l-3 3m3-3v6m-1.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z">
-                                </path>
-                            </svg>
-                            <a href="http://127.0.0.1:8000/user/contribution"
-                                class="text-lg font-medium text-gray-700 hover:text-teal-500 transition-colors duration-200">
-                                Your contribution
-                            </a>
-                        </li>
-
-                        <li class="flex items-center gap-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="w-6 h-6 text-teal-500">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z">
-                                </path>
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z">
-                                </path>
-                            </svg>
-                            <a href="http://127.0.0.1:8000/user/settings"
-                                class="text-lg font-medium text-gray-700 hover:text-blue-500 transition-colors duration-200">
-                                Settings
-                            </a>
-                        </li>
-
-                        <form action="http://127.0.0.1:8000/auth/logout" method="POST" class="mt-4">
-                            <input type="hidden" name="_token" value="ymEkCLBFpgkdaSbidUArRsdHbER5DkT6ByS3eJYb">
-                            <button type="submit"
-                                class="w-full text-center text-red-600 text-base font-medium px-4 py-2 hover:bg-red-200 transition-colors duration-200 rounded-md">
-                                Log Out
-                            </button>
-                        </form>
-                    </ul>
-                </div>
-
-
-                <div class="bg-slate-100 p-4 rounded-lg shadow-md">
-                    <h2 class="text-xl font-semibold text-gray-900 mb-4">Recommended Subreddits</h2>
-                    <ul role="list" class="divide-y divide-gray-200">
-                        <li class="flex items-center justify-between gap-x-4 py-3">
-                            <div class="flex items-center gap-x-4">
-                                <img class="h-12 w-12 rounded-full object-cover"
-                                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                    alt="Subreddit Icon">
-                                <div>
-                                    <p class="text-sm font-semibold text-gray-900">Leslie Alexander</p>
-                                    <p class="text-xs text-gray-500">r/SubredditName</p>
-                                </div>
-                            </div>
-                            <button type="button" class="text-sm font-medium text-teal-600 hover:text-teal-800">
-                                Follow
-                            </button>
-                        </li>
-                        <li class="flex items-center justify-between gap-x-4 py-3">
-                            <div class="flex items-center gap-x-4">
-                                <img class="h-12 w-12 rounded-full object-cover"
-                                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                    alt="Subreddit Icon">
-                                <div>
-                                    <p class="text-sm font-semibold text-gray-900">Leslie Alexander</p>
-                                    <p class="text-xs text-gray-500">r/SubredditName</p>
-                                </div>
-                            </div>
-                            <button type="button" class="text-sm font-medium text-teal-600 hover:text-teal-800">
-                                Follow
-                            </button>
-                        </li>
-                        <li class="flex items-center justify-between gap-x-4 py-3">
-                            <div class="flex items-center gap-x-4">
-                                <img class="h-12 w-12 rounded-full object-cover"
-                                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                    alt="Subreddit Icon">
-                                <div>
-                                    <p class="text-sm font-semibold text-gray-900">Leslie Alexander</p>
-                                    <p class="text-xs text-gray-500">r/SubredditName</p>
-                                </div>
-                            </div>
-                            <button type="button" class="text-sm font-medium text-teal-600 hover:text-teal-800">
-                                Follow
-                            </button>
-                        </li>
-                    </ul>
-                </div>
-
-            </div>
+            <SidebarMenu />
         </div>
         <div class="w-full md:w-1/2 bg-white p-4">
             <!-- Add your main content here -->
@@ -321,3 +187,8 @@
         </div>
     </div>
 </template>
+
+
+<script setup lang="ts">
+
+</script>
